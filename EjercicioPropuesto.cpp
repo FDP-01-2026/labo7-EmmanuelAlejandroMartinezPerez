27cc1c2018254dea1d508e2f1f73b8f78b0d01e6
EmmanuelAlejandroMartinezPerez <00092626@uca.edu.sv>
2026-05-19 16:36:42 -0600

    Ejercicios Propuestos Laboratorio 7😎

--- EjercicioPropuesto.cpp
+++ EjercicioPropuesto.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 
 void ContarImpares(int *Num);
-void ElevarValores(int *NumX, int *NumY);
+void ElevarValores(int *NumX, int *NumY, long long *Resultado);
+
 
 int main () {
-    int Num;
+
+int Num;
+int NumX, NumY;
+long long Resultado = 1;
 
     ContarImpares(&Num);
+cout << "Ahora se le pedira ingresar dos numeros, el primero sera el numero base y el segundo sera el exponente, se mostrara el resultado de elevar el numero base al exponente" << endl;
+    ElevarValores(&NumX, &NumY, &Resultado);
 
 
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void ContarImpares(int *Num) {
 
@@ -19,6 +27,20 @@ cin >> *Num;
 
 for (int i = 1; i < *Num; i+=2) 
 { 
-    cout << i << " ";
+    cout << i << " " << endl;
 }
+}
+
+void ElevarValores(int *NumX, int *NumY, long long *Resultado) {
+
+    cout << "Ingrese el primer numero, que sera el numero base" << endl;
+    cin >> *NumX;
+    cout << "Ingrese el segundo numero, que sera el exponente" << endl;
+    cin >> *NumY;
+
+    for (int i = 1; i <= *NumY; i++) 
+    { 
+        *Resultado *= *NumX;
+    }
+    cout << "El resultado de elevar " << *NumX << " a la " << *NumY << " es: " << *Resultado << endl;
 }
